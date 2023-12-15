f7e45966ea72ac0644f9157406cca73bc0a46061
Uallessonivo <uallessons@gmail.com>
2023-12-15 16:44:00 -0300

    Implemented the extract files method

--- src/main/java/com/project/dbbackupsolution/domain/services/FileServiceImpl.java
+++ src/main/java/com/project/dbbackupsolution/domain/services/FileServiceImpl.java
@@ -1,29 +1,51 @@
 package com.project.dbbackupsolution.domain.services;
 
 import com.project.dbbackupsolution.configuration.LoadPaths;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class FileServiceImpl implements FileService {
     private final LoadPaths loadPaths;
 
+    @Autowired
     public FileServiceImpl(LoadPaths loadPaths) {
         this.loadPaths = loadPaths;
     }
 
     @Override
     public List<File> extractFiles(String path, String fileExtension) {
-        return null;
+        File folder = new File(path);
+
+        if (!folder.exists()) {
+            throw new IllegalArgumentException("The path does not exist");
+        }
+
+        File[] listOfFiles = folder.listFiles();
+        List<File> filesWithDesiredExtension = new ArrayList<>();
+
+        if (listOfFiles == null) {
+            throw new IllegalArgumentException("The path does not contain any files");
+        }
+
+        for (File file : listOfFiles) {
+            if (file.isFile() && file.getName().endsWith(fileExtension)) {
+                filesWithDesiredExtension.add(file);
+            }
+        }
+
+        return filesWithDesiredExtension;
     }
 
     private void extractMetadata(String path, String fileExtension) {
 
     }
 
-    private void compressFiles(String path, String fileExtension) {
-
+    private File compressFile(File file) {
+        return null;
     }
 }
