4afd42ae66e56ba9681915847831b32902e1daa8
Uallessonivo <uallessons@gmail.com>
2023-12-18 14:23:26 -0300

    Implemented the sendFile endpoint to the file controller

---
+++ src/main/java/com/project/dbbackupsolution/api/controllers/FileController.java
@@ -0,0 +1,26 @@
+package com.project.dbbackupsolution.api.controllers;
+
+import com.project.dbbackupsolution.api.request.FileRequest;
+import com.project.dbbackupsolution.domain.services.FileService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/files")
+public class FileController {
+    private final FileService fileService;
+
+    public FileController(FileService fileService) {
+        this.fileService = fileService;
+    }
+
+    @PostMapping("/send")
+    public ResponseEntity<String> sendFile(@RequestBody FileRequest fileRequest) {
+        try {
+            fileService.sendFileToStorage(fileRequest.getPath(), fileRequest.getFileExtension());
+            return ResponseEntity.ok("Files sent successfully");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+}
