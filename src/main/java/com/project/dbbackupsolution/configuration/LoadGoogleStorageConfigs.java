79ef6d867f188c3c9c846b2f27ac0fd6534ef58f
Uallessonivo <uallessons@gmail.com>
2023-12-21 15:50:16 -0300

    Refactored  the config class

--- src/main/java/com/project/dbbackupsolution/configuration/LoadGoogleStorageConfigs.java
+++ src/main/java/com/project/dbbackupsolution/configuration/LoadGoogleStorageConfigs.java
@@ -1,18 +1,19 @@
 package com.project.dbbackupsolution.configuration;
 
 import lombok.Getter;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-
 
 @Getter
 @Configuration
 public class LoadGoogleStorageConfigs {
-    @Value("${google.storage.project-id}")
     private String projectId;
-    @Value("${google.storage.bucket-name}")
     private String bucketName;
-    @Value("${google.storage.credentials}")
     private String credentials;
     private final String backupPath = "backup/";
+
+    public LoadGoogleStorageConfigs() {
+        this.projectId = System.getenv("GOOGLE_STORAGE_PROJECT_ID");
+        this.bucketName = System.getenv("GOOGLE_STORAGE_BUCKET_NAME");
+        this.credentials = System.getenv("GOOGLE_APPLICATION_CREDENTIALS");
+    }
 }
--- src/main/java/com/project/dbbackupsolution/configuration/LoadPaths.java
+++ src/main/java/com/project/dbbackupsolution/configuration/LoadPaths.java
@@ -1,14 +1,16 @@
 package com.project.dbbackupsolution.configuration;
 
 import lombok.Getter;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 @Getter
 @Configuration
 public class LoadPaths {
-    @Value("${server.storage.backup-path}")
     private String BACKUP_PATH;
-    @Value("${server.storage.restore-path}")
     private String RESTORE_PATH;
+
+    public LoadPaths() {
+        BACKUP_PATH = System.getenv("SERVER_STORAGE_BACKUP_PATH");
+        RESTORE_PATH = System.getenv("SERVER_STORAGE_RESTORE_PATH");
+    }
 }
