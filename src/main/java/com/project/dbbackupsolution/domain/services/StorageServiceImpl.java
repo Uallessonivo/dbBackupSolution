da97447faac4fae24624969e37359f1e9aa228bc
Uallessonivo <uallessons@gmail.com>
2024-01-24 14:14:41 -0300

    Added initial logging and logback.xml configuration file

--- src/main/java/com/project/dbbackupsolution/domain/services/StorageServiceImpl.java
+++ src/main/java/com/project/dbbackupsolution/domain/services/StorageServiceImpl.java
@@ -6,6 +6,9 @@ import com.project.dbbackupsolution.configuration.LoadGoogleStorageConfigs;
 import com.project.dbbackupsolution.domain.exceptions.DomainException;
 import com.project.dbbackupsolution.domain.exceptions.FileException;
 import com.project.dbbackupsolution.infrastructure.GoogleStorage;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -19,6 +22,7 @@ import java.util.Objects;
 
 @Service
 public class StorageServiceImpl implements StorageService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(StorageServiceImpl.class);
     private final LoadGoogleStorageConfigs storageConfig;
     private final Storage storage;
 
@@ -29,25 +33,35 @@ public class StorageServiceImpl implements StorageService {
 
     @Override
     public void moveFile(String fileName, String destinationPath) {
+        LOGGER.info("Moving file {} to {}", fileName, destinationPath);
+
         try {
             Blob file = copyObjectFromBucket(fileName, destinationPath);
             file.delete();
+            LOGGER.info("File {} moved successfully", fileName, destinationPath);
         } catch (Exception e) {
+            LOGGER.error("Error while moving file ", e);
             throw new DomainException("Error while moving file ", e.getCause());
         }
     }
 
     @Override
     public void copyFile(String fileName, String destinationPath) {
+        LOGGER.info("Copying file {} to {}", fileName, destinationPath);
+
         try {
             copyObjectFromBucket(fileName, destinationPath);
+            LOGGER.info("File {} copied successfully", fileName, destinationPath);
         } catch (Exception e) {
+            LOGGER.error("Error while copying file ", e);
             throw new DomainException("Error while copying file ", e.getCause());
         }
     }
 
     @Override
     public void uploadFile(MultipartFile file) {
+        LOGGER.info("Uploading file {}", file.getOriginalFilename());
+
         try {
             String fullPath = storageConfig.getBackupPath() + file.getOriginalFilename();
             BlobId blobId = BlobId.of(storageConfig.getBucketName(), fullPath);
@@ -56,7 +70,9 @@ public class StorageServiceImpl implements StorageService {
                     .build();
 
             storage.create(blobInfo, file.getBytes());
+            LOGGER.info("File {} uploaded successfully", file.getOriginalFilename());
         } catch (Exception e) {
+            LOGGER.error("Error while uploading file ", e);
             throw new FileException("Error occurred while backing up file: ", file.getOriginalFilename());
         }
     }
@@ -66,6 +82,8 @@ public class StorageServiceImpl implements StorageService {
         if (file == null) {
             throw new IllegalArgumentException("File or filename cannot be null");
         }
+
+        LOGGER.info("Sending file {}", file.getName());
 
         try {
             String fileName = file.getName();
@@ -82,13 +100,16 @@ public class StorageServiceImpl implements StorageService {
             }
 
             storage.create(blobInfo, bytesArray);
+            LOGGER.info("File {} sent successfully", file.getName());
         } catch (Exception e) {
+            LOGGER.error("Error while sending file ", e);
             throw new FileException("Error occurred while sending file: ", file.getName());
         }
     }
 
     @Override
     public void backupFiles(MultipartFile[] files) {
+        LOGGER.info("Backing up {} files", files.length);
         List<String> failedFiles = new ArrayList<>();
 
         for (MultipartFile file : files) {
@@ -105,25 +126,35 @@ public class StorageServiceImpl implements StorageService {
             } catch (Exception e) {
                 failedFiles.add(file.getOriginalFilename());
             }
+            LOGGER.info("File {} backed up successfully", file.getOriginalFilename());
         }
 
         if (!failedFiles.isEmpty()) {
+            LOGGER.error("Error while backing up files ", failedFiles);
             throw new FileException("Error occurred while backing up some files", failedFiles);
         }
+
+        LOGGER.info("All files backed up successfully");
     }
 
     @Override
     public void deleteFile(String fileName) {
+        LOGGER.info("Deleting file {}", fileName);
+
         try {
             Blob blob = storage.get(storageConfig.getBucketName(), fileName);
             blob.delete();
+            LOGGER.info("File {} deleted successfully", fileName);
         } catch (Exception e) {
+            LOGGER.error("Error while deleting file ", e);
             throw new FileException("Error occurred while deleting file: ", fileName);
         }
     }
 
     @Override
     public void deleteOldFiles(int numberOfDays) {
+        LOGGER.info("Deleting files older than {} days", numberOfDays);
+
         LocalDate currentDate = LocalDate.now();
         LocalDate daysAgo = currentDate.minusDays(numberOfDays);
         List<String> failedFiles = new ArrayList<>();
@@ -135,19 +166,26 @@ public class StorageServiceImpl implements StorageService {
             if (blobCreationDate.isBefore(daysAgo)) {
                 try {
                     blob.delete();
+                    LOGGER.info("File {} deleted successfully", blob.getName());
                 } catch (Exception e) {
+                    LOGGER.error("Error while deleting file ", e);
                     failedFiles.add(blob.getName());
                 }
             }
         }
 
         if (!failedFiles.isEmpty()) {
+            LOGGER.error("Error while deleting files ", failedFiles);
             throw new FileException("Error occurred while deleting some files", failedFiles);
         }
+
+        LOGGER.info("All files older than {} days deleted successfully", numberOfDays);
     }
 
     @Override
     public void deleteAllFilesByExtension(String fileExtension) {
+        LOGGER.info("Deleting all files with extension {}", fileExtension);
+
         List<String> failedFiles = new ArrayList<>();
         if (!fileExtension.startsWith(".")) {
             fileExtension = "." + fileExtension;
@@ -159,19 +197,26 @@ public class StorageServiceImpl implements StorageService {
             if (fileName.endsWith(fileExtension)) {
                 try {
                     blob.delete();
+                    LOGGER.info("File {} deleted successfully", fileName);
                 } catch (Exception e) {
                     failedFiles.add(fileName);
+                    LOGGER.error("Error while deleting file ", e);
                 }
             }
         }
 
         if (!failedFiles.isEmpty()) {
+            LOGGER.error("Error while deleting files ", failedFiles);
             throw new FileException("Error occurred while deleting some files", failedFiles);
         }
+
+        LOGGER.info("All files with extension {} deleted successfully", fileExtension);
     }
 
     @Override
     public void deleteAllOldFilesByExtension(int numberOfDays, String fileExtension) {
+        LOGGER.info("Deleting all files with extension {} older than {} days", fileExtension, numberOfDays);
+
         LocalDate currentDate = LocalDate.now();
         LocalDate daysAgo = currentDate.minusDays(numberOfDays);
 
@@ -188,36 +233,49 @@ public class StorageServiceImpl implements StorageService {
             if (fileName.endsWith(fileExtension) && blobCreationDate.isBefore(daysAgo)) {
                 try {
                     blob.delete();
+                    LOGGER.info("File {} deleted successfully", fileName);
                 } catch (Exception e) {
+                    LOGGER.error("Error while deleting file ", e);
                     failedFiles.add(fileName);
                 }
             }
         }
 
         if (!failedFiles.isEmpty()) {
+            LOGGER.error("Error while deleting files ", failedFiles);
             throw new FileException("Error occurred while deleting some files", failedFiles);
         }
-    }
 
+        LOGGER.info("All files with extension {} older than {} days deleted successfully", fileExtension, numberOfDays);
+    }
 
     @Override
     public ByteArrayResource downloadFile(String fileName) {
+        LOGGER.info("Downloading file {}", fileName);
+
         try {
             Blob file = searchObject(fileName);
             byte[] content = file.getContent();
+            LOGGER.info("File {} downloaded successfully", fileName);
             return new ByteArrayResource(content);
         } catch (Exception e) {
+            LOGGER.error("Error while downloading file ", e);
             throw new DomainException("Error occurred while downloading file: " + fileName);
         }
     }
 
     @Override
     public List<String> listOfFiles() {
+        LOGGER.info("Listing all files");
+
         List<String> list = new ArrayList<>();
         Page<Blob> blobs = storage.list(storageConfig.getBucketName());
         for (Blob blob : blobs.iterateAll()) {
             list.add(blob.getName());
         }
+
+        LOGGER.info("Listed {} files", list.size());
+
         return list;
     }
 
